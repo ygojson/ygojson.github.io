3c7b2af623a5a6918553c8c402157f7b7a378c96
magicDGS <daniel.gomez.sanchez@hotmail.es>
2023-10-22 21:50:18 +0200

    Add site URL to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ygojson.github.io',
 	integrations: [
 		starlight({
 			title: 'My Docs',
